8c197ea7be34789d421d083206d99d9393bbde81
thanhapec1702 <135965488+thanhapec1702@users.noreply.github.com>
2025-01-14 16:25:39 +0700

    update

---
+++ src/main/java/com/apec_finance/cash/repository/InvestorCashBalanceHistoryRepository.java
@@ -0,0 +1,20 @@
+package com.apec_finance.cash.repository;
+
+import com.apec_finance.cash.entity.CsInvestorCashBalanceHistoryEntity;
+
+import java.time.LocalDate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+
+public interface InvestorCashBalanceHistoryRepository extends JpaRepository<CsInvestorCashBalanceHistoryEntity, Long> {
+    @Query("SELECT c FROM CsInvestorCashBalanceHistoryEntity c WHERE c.investorId = :investorId AND c.tradingDate = :tradingDate and c.status = 'A' and c.deleted = 0")
+    CsInvestorCashBalanceHistoryEntity findByInvestorIdAndTradingDate(@Param("investorId") Long investorId, @Param("tradingDate") LocalDate tradingDate);
+    @Query("SELECT c FROM CsInvestorCashBalanceHistoryEntity c WHERE c.investorId = :investorId AND c.status = :status and c.deleted = 0")
+    CsInvestorCashBalanceHistoryEntity findByInvestorIdAndStatus(@Param("investorId") Long investorId, @Param("status") String status);
+
+}
+
+
--- src/main/java/com/apec_finance/cash/repository/InvestorCashTransactionRepository.java
+++ src/main/java/com/apec_finance/cash/repository/InvestorCashTransactionRepository.java
@@ -4,10 +4,18 @@ import com.apec_finance.cash.entity.InvestorCashTransactionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import java.util.List;
 
 import java.time.LocalDate;
 
 public interface InvestorCashTransactionRepository extends JpaRepository<InvestorCashTransactionEntity, Long> {
     @Query("SELECT COUNT(t) FROM InvestorCashTransactionEntity t WHERE t.tranDate = :tranDate AND t.tranType = :tranType")
-    int countTransactionsByDateAndType(@Param("tranDate") LocalDate tranDate, @Param("tranType") String tranType);
+    int countTransactionsByDateAndType(@Param("tranDate") LocalDate tranDate , @Param("tranType") String tranType);
+    @Query("SELECT t FROM InvestorCashTransactionEntity t WHERE t.tranDate >= :startDate AND t.tranDate <= :endDate AND t.tranType IN :tranTypes AND t.investorId = :investorId")
+    List<InvestorCashTransactionEntity> findTransactionsByDateTypeAndInvestor(
+        @Param("investorId") Long investorId,
+        @Param("startDate") LocalDate startDate,
+        @Param("endDate") LocalDate endDate,
+        @Param("tranTypes") List<String> tranTypes
+    );
 }
